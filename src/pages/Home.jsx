163ba36539fdd9898afb6ac29561319153e0358b
Sabyasachi Bose <137913350+84-bosesabyasachi@users.noreply.github.com>
2025-12-24 00:47:46 +0530

    Change developer description in Home component
    Updated the description of the developer role in the hero section.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
           </div>
           <div className="hero-text">
             <p>
-              As a skilled full-stack developer, I am dedicated to turning ideas
+              As a skilled MERN stack developer, I am dedicated to turning ideas
               into innovative web applications. Explore my latest projects and
               articles, showcasing my expertise in React.js and web development.
             </p>
@@ -199,4 +199,5 @@ const StyledHome = styled(motion.div)`
   }
 `;
 export default Home;
+
 
